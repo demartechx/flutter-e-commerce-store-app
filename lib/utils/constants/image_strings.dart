e07f15334a47f078e0e3e21272ab6e56a48e3dfc
Adeagbo Matthew Damilare <demartechx@Adeagbos-MacBook-Pro.local>
2024-09-20 12:01:35 +0100

    utils

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,12 @@
+
+
+class TImages {
+
+  //app logos
+  static String darkAppLogo = "assets/logos/t-store-splash-logo-black.png";
+  static String lightAppLogo = "assets/logos/t-store-splash-logo-white.png";
+
+  // social logos
+  static String google = "assets/logos/google-icon.png";
+  static String facebook = "assets/logos/facebook-icon.png";
+}
---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,19 @@
+
+
+class TTexts {
+
+  // onboarding texts 
+  static String onBoardingTitle1 = "Choose your product";
+  static String onBoardingTitle2 = "Select Payment Method";
+  static String onBoardingTitle3 = "Deliver at your door step";
+
+
+  static String onBoardingSubTitle1 = "Welcome to a World of limitless Choices - Your Perfect Product Awaits!";
+  static String onBoardingSubTitle2 = "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!";
+  static String onBoardingSubTitle3 = "From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery";
+
+
+  //home
+  static String homeAppbarTitle = "Good day for shopping";
+  static String homeAppbarSubTitle = "Adeagbo Matthew";
+}
