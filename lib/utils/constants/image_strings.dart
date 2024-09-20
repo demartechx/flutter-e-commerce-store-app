393ee60d50d10a5644119a54a8cb67e595743f04
Adeagbo Matthew Damilare <demartechx@Adeagbos-MacBook-Pro.local>
2024-09-21 00:52:12 +0100

    onboarding screen

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -9,4 +9,10 @@ class TImages {
   // social logos
   static String google = "assets/logos/google-icon.png";
   static String facebook = "assets/logos/facebook-icon.png";
+
+  // onboarding images
+  static String onBoardingImage1 = "assets/images/on_boarding_images/sammy-line-searching.gif";
+  static String onBoardingImage2 = "assets/images/on_boarding_images/sammy-line-shopping.gif";
+  static String onBoardingImage3 = "assets/images/on_boarding_images/sammy-line-delivery.gif";
+
 }
