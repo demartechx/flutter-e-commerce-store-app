e2bc300e6acfba70584f57ba1afa4b37522dae58
Adeagbo Matthew Damilare <demartechx@Adeagbos-MacBook-Pro.local>
2024-09-22 21:58:42 +0100

    reset password

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:t_store/features/authentication/screens/password_configuration/forgot_password.dart';
 import 'package:t_store/features/authentication/screens/signup/signup.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
@@ -54,7 +55,7 @@ class TLoginForm extends StatelessWidget {
     
               //forgot password
               TextButton(
-                  onPressed: () {},
+                  onPressed: () => Get.to(()=> const ForgetPassword()),
                   child: const Text(TTexts.forgetPassword))
             ],
           ),
