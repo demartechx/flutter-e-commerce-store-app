07002aa384d4317906e07319e1f70e431acaeebe
Adeagbo Matthew Damilare <demartechx@Adeagbos-MacBook-Pro.local>
2024-09-23 14:53:28 +0100

    search bar and horizontal listview

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,21 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:t_store/common/widgets/image_text_widgets/vertical_image_text.dart';
+import 'package:t_store/common/widgets/text/section_heading.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_appbar.dart';
-
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
-        child:  Column(
+        child: Column(
           children: [
+            //header
             TPrimaryHeaderContainer(
               child: Column(
                 children: [
-                  THomeAppBar()
+                  //appbar
+                  const THomeAppBar(),
+                  SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+
+                  //searchbar
+                  const TSearchContainer(text: 'Search in Store'),
+                  SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
+
+                  //categories
+                  Padding(
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        //heading
+                        const TSectionHeading(
+                            title: 'Popular Categories',
+                            showActionButton: false,
+                            textColor: Colors.white,
+                            ),
+                        SizedBox(
+                          height: TSizes.spaceBtwItems,
+                        ),
+
+                        //categories
+                        SizedBox(
+                          height: 80,
+                          child: ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: 6,
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (_, index) {
+                                return  TVerticalImageText(image: TImages.shoeIcon, title: 'Shoes Category', onTap: (){});
+                              }),
+                        )
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
@@ -25,6 +70,5 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
 
 
